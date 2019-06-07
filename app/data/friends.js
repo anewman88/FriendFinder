4ef2e74b92ad93db772c6abff6b731175f8b33ed
Ann Newman <anewman1822@gmail.com>
2019-06-07 18:45:08 -0500

    added logic to make friend match. added questions.

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,8 +4,72 @@ var friendsList = [
 		name: "Scooter",
 		photo: "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg",
 		scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+	},
+	{
+		name: "Carrie",
+		photo: "https://cps-static.rovicorp.com/3/JPG_400/MI0004/410/MI0004410626.jpg?partner=allrovi.com",
+		scores: [2, 3, 5, 3, 4, 2, 2, 4, 3, 2]
+	},
+	{
+		name: "Blake",
+		photo: "http://images.tritondigitalcms.com/6616/sites/380/2017/11/08094354/Blake-Shelton-110817-630x420.png",
+		scores: [5, 4, 3, 2, 1, 5, 4, 3, 2, 1]
+	},
+	{
+		name: "Phoebe",
+		photo: "https://scottandkat.ca/wp-content/uploads/2017/06/phoebe.jpg",
+		scores: [2, 1, 3, 1, 2, 4, 1, 4, 1, 3]
+	},
+	{
+		name: "Joey",
+		photo: "https://vignette.wikia.nocookie.net/friends/images/b/b2/Joey_Friends.png/revision/latest/scale-to-width-down/200?cb=20160528181559",
+		scores: [1, 3, 1, 5, 3, 2, 5, 1, 4, 1]
+	},
+	{
+		name: "Rachel",
+		photo: "https://cdn-img.instyle.com/sites/default/files/styles/684xflex/public/images/2014/WRN/050514-jennifer-aniston-594.jpg",
+		scores: [4, 2, 2, 2, 1, 5, 1, 2, 3, 2]
+	},
+	{
+		name: "Ross",
+		photo: "https://vignette.wikia.nocookie.net/friends/images/0/0b/RossGeller.jpg/revision/latest?cb=20180424154547",
+		scores: [3, 1, 5, 2, 3, 5, 1, 5, 5, 2]
+	},
+	{
+		name: "Monica",
+		photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-friends-courteney-cox.jpg",
+		scores: [5, 1, 3, 2, 5, 2, 1, 2, 3, 2]
+	},
+	{
+		name: "Chandler",
+		photo: "https://vignette.wikia.nocookie.net/friends/images/c/cc/Square_Chandler.jpg/revision/latest/scale-to-width-down/200?cb=20111216200026",
+		scores: [5, 1, 4, 2, 4, 5, 1, 4, 3, 2]
+	},
+	{
+		name: "Jerry",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdAFmKIb7O2MeyWPZ7qUcfrTok9NIWj2O6QjVfZuGeEF6y2EP8Bw",
+		scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	},
+	{
+		name: "George",
+		photo: "https://abovethelaw.com/uploads/2017/08/George_Costanza-e1503190598605-300x247.jpg",
+		scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+	},
+	{
+		name: "Elaine",
+		photo: "https://akns-images.eonline.com/eol_images/Entire_Site/2016912/rs_671x1024-161012104936-634.Julia-Louis-Dreyfus-Seinfeld.ms.101216.jpg",
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},
+	{
+		name: "Kramer",
+		photo: "http://www.media1.hw-static.com/media/2016/12/wenn_michaelrichards_kramer_seinfeld_122716_1800x1200-800x533.jpg",
+		scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+	},
+	{
+		name: "Newman",
+		photo: "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/182750_156283314427338_3692924_n.jpg?_nc_cat=101&_nc_ht=scontent-dfw5-1.xx&oh=2417df3b1b2c46cbd5ce0f015a5992e7&oe=5D9016BB",
+		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 	}
 ]
-
 // Export to API Route
 module.exports = friendsList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -35,33 +35,57 @@ module.exports = function(app) {
         // This works because of the body parsing middleware
         var newFriend = request.body;
         var newScores = request.body.scores;
+        var bestMatchValue = 41; // make larger than largest possible value
+        var bestMatchIndex = 0;  // default index value
         
-        // Temp store new friend input 
+        // Temp store new friend input.  Easier to work with
         var newFriend = {
             name: request.body.name,
             photo: request.body.photo,
             scores: []
         };
-        // Convert scores from string to integer data (AJAX post seemed to make the numbers strings)
+        // Convert scores from string to integer data (post seemed to make the numbers strings)
         newFriend.scores = newScores.map(Number);
         if (debugON) console.log("New Friend ", newFriend);
 
         // Determine the best match
-        
+        // loop through the friendList
+        for (var i = 0; i< friendList.length; i++) {
+            // get the current friend in the list
+            var currentFriend = {
+                name: friendList[i].name,
+                photo: friendList[i].photo,
+                scores: friendList[i].scores
+            };
 
-        //
+            console.log (currentFriend.scores, newFriend.scores);
+            // Compare each survey scores of the currentFriend with the scores of newFriend
+            // calculate the absolute value between the scores and add it
+            // to the curMatchValue;
+            var curMatchValue = 0;
+            currentFriend.scores.forEach(function calcMatchValue(score, index) {
+                console.log ("difference is " + Math.abs (newFriend.scores[index] - score))
+                curMatchValue += (Math.abs (newFriend.scores[index] - score));
+            });
+
+            // Check if current friend is the new best match
+            if (debugON) console.log ("curMatchValue = " + curMatchValue);
+            if (curMatchValue <= bestMatchValue) {
+                bestMatchValue = curMatchValue;
+                bestMatchIndex = i;
+                if (debugON) console.log ("bestMatchValue = "+ bestMatchValue +" bestMatchIndex = " + bestMatchIndex);
+            }  // if
+        }  // for 
 
         // Add the user input data to the friendList
         // wait to push the new user data before finding the best match otherwise
         // the best match will be the newly input friend.
         friendList.push(newFriend);
         if (debugON) console.log ("just pushed new friend ", newFriend);
-        //  For now return the new friend data
-        result.json(newFriend);
-
+    
+        if (debugON) console.log ("returning best match friend ", friendList[bestMatchIndex]);
         //  Return the best match data
-        //result.json(bestMatch);
-
+        result.json(friendList[bestMatchIndex]);
         
     });  //  app.post("/api/friends"...
     
